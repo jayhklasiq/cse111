e9817482d3e5e65001ab1607005855281121b4ca
jayhklasiq <klasik@byui.edu>
2023-02-02 12:20:28 +0000

    team assignment done

--- wk5/code_files/test_names.py
+++ wk5/code_files/test_names.py
@@ -1,3 +1,23 @@
 from names import make_full_name, \
     extract_family_name, extract_given_name
 import pytest
+
+def test_make_full_name():
+    assert make_full_name('Joshua', 'Olaoye')
+    assert make_full_name('Edudzi', 'Agbeko')
+    assert make_full_name('Elijah', 'Nyamkyere')
+    
+def test_extract_family_name():
+    assert extract_family_name('Elijah; Nyamkyere')
+    assert extract_family_name('Joshua; Olaoye')
+    assert extract_family_name('Edudzi; Agbeko')
+        
+def test_extract_given_name():
+    assert extract_given_name('Elijah/ Nyamkyere')
+    assert extract_given_name('Elijah/ Nyamkyere')
+    assert extract_given_name('Elijah/ Nyamkyere')
+    
+       
+# Call the main function that is part of pytest so that the
+# computer will execute the test functions in this file.
+pytest.main(["-v", "--tb=line", "-rN", __file__])
