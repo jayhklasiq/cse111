173b27bb17d6ca4d4a63fa652ebb841ba8b688a2
jayhklasiq <klasik@byui.edu>
2023-02-02 20:02:57 +0000

    week 5

--- wk5/code_files/test_names.py
+++ wk5/code_files/test_names.py
@@ -3,19 +3,19 @@ from names import make_full_name, \
 import pytest
 
 def test_make_full_name():
-    assert make_full_name('Joshua', 'Olaoye')
-    assert make_full_name('Edudzi', 'Agbeko')
-    assert make_full_name('Elijah', 'Nyamkyere')
+    assert make_full_name('Olaoye', 'Joshua') == 'Joshua; Olaoye'
+    assert make_full_name('Agbeko', 'Edudzi') == 'Edudzi; Agbeko'
+    assert make_full_name('Nyamkyere', 'Elijah') == 'Elijah; Nyamekyere'
     
 def test_extract_family_name():
-    assert extract_family_name('Elijah; Nyamkyere')
-    assert extract_family_name('Joshua; Olaoye')
-    assert extract_family_name('Edudzi; Agbeko')
+    assert extract_family_name('Elijah; Nyamkyere') == 'Elijah'
+    assert extract_family_name('Joshua; Olaoye') == 'Joshua'
+    assert extract_family_name('Edudzi; Agbeko') == 'Edudzi'
         
 def test_extract_given_name():
-    assert extract_given_name('Elijah/ Nyamkyere')
-    assert extract_given_name('Elijah/ Nyamkyere')
-    assert extract_given_name('Elijah/ Nyamkyere')
+    assert extract_given_name('Elijah/ Nyamkyere') == 'Nyamkyere'
+    assert extract_given_name('Edudzi/ Agbeko') == 'Agbeko'
+    assert extract_given_name('Joshua/ Olaoye') == 'Olaoye'
     
        
 # Call the main function that is part of pytest so that the
